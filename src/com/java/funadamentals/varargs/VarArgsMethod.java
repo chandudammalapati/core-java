340bbccf464ca57fa3fbc3d0c8e08fb2d1b52cc9
Dammalapati <dammalapati@Dammalapatis-MacBook-Pro.local>
2019-06-01 22:33:45 -0700

    adding same method with single int arguments to check which one takes precedence

--- src/com/java/funadamentals/varargs/VarArgsMethod.java
+++ src/com/java/funadamentals/varargs/VarArgsMethod.java
@@ -15,7 +15,9 @@ public class VarArgsMethod {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		m1(10, 20, 30);
-
+		m1(12, 32);
+		m1();
+		m1(43);
 	}
 	
 	public static void m1(int... x) {
@@ -23,6 +25,10 @@ public class VarArgsMethod {
 		for (int x1 : x) {
 			total = total+x1;
 		}
-		System.out.println("total is "+total);
+		System.out.println("var args total is "+total);
+	}
+	
+	public static void m1(int x) {
+		System.out.println("general method output is "+x);
 	}
 }
