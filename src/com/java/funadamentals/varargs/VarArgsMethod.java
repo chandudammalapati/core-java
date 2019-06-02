cbaf16a1b36668c897b500144fd0666fe76bdb41
Dammalapati <dammalapati@Dammalapatis-MacBook-Pro.local>
2019-06-01 22:18:36 -0700

    varargs class initial commit

---
+++ src/com/java/funadamentals/varargs/VarArgsMethod.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.java.funadamentals.varargs;
+
+/**
+ * @author dammalapati
+ *
+ */
+public class VarArgsMethod {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
