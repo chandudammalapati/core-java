3c8525347e6adbb46614120be7d6b8e2675d4d6c
Dammalapati <dammalapati@Dammalapatis-MacBook-Pro.local>
2019-06-01 22:25:19 -0700

    using vararg method ... and adding up sum of 3 integers

--- src/com/java/funadamentals/varargs/VarArgsMethod.java
+++ src/com/java/funadamentals/varargs/VarArgsMethod.java
@@ -14,7 +14,15 @@ public class VarArgsMethod {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		m1(10, 20, 30);
 
 	}
-
+	
+	public static void m1(int... x) {
+		int total = 0;
+		for (int x1 : x) {
+			total = total+x1;
+		}
+		System.out.println("total is "+total);
+	}
 }
